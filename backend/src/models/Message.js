c2cb7d5ef7be5f033c8b994417bb98820046a4e5
Muskan17361 <136142613+Muskan17361@users.noreply.github.com>
2025-12-10 11:52:14 +0530

    Remove empty line at the beginning of Message.js

---
+++ backend/src/models/Message.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const MessageSchema = new mongoose.Schema({
+  from: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  to: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+  job: { type: mongoose.Schema.Types.ObjectId, ref: 'Job' },
+  text: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+  read: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model('Message', MessageSchema);
